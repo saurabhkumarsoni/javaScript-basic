4d678bb0e89dfad3884a57926fa2affed7cbf23c
SAURABH KUMAR <saurabh.soni119@gmail.com>
2024-04-09 07:48:38 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,25 @@
+const accountId = 144553;
+let accountEmail = "saurabh.soni119@gmail.com";
+var accountPassword = "123";
+accountCity = "Bangalore";
+
+let accountState;
+
+
+
+//accountId = 2;  // not allowed
+
+
+accountEmail = 'hk@gmail.com'
+accountPassword = '321'
+accountCity = "Aurangabad"
+
+
+console.log(accountId);
+
+/*
+    Prefer not to use var
+    because of issue in block scope and functional scope
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+
+// alert('hello')   // we are using node js not browser
+
+
+let name = 'saurabh'
+let age = 30
+let isAdmin = false
+
+
+// number  =>2 to power 53
+// biginit
+// string = ''
+// boolean =>false /true
+// null
+// undefined
+// symbol => unique
+
+
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null)       // object
