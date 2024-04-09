68a808dfc6a61017bfa182aba8b0814b9a23393b
SAURABH KUMAR <saurabh.soni119@gmail.com>
2024-04-09 08:40:43 +0000

    conversion and operation done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,77 @@
+let score = "33abc";
+
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "22abc" => NaN
+// true => 1, false => 0
+
+
+let isAdmin = 'saurabh'
+let booleanIsAdmin = Boolean(isAdmin);
+
+console.log(booleanIsAdmin);
+
+
+//  1 => true, 0 => false
+// '' => false;
+// 'saurabh' => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber + 33)
+
+
+// 33 => string now
+
+
+// ****************************************************operations********************************************
+
+let value = 3
+let negativeValue = -value
+// console.log(negativeValue) // -3
+
+
+
+
+let str1 = 'Hello'
+let str2 = ' Saurabh'
+
+let str3 = str1 + str2
+// console.log(str3)  // Hello Saurabh
+
+console.log("1" + 1);     // 11
+console.log(1 + "2");     // 12
+console.log("1" + "2");   // 12
+console.log("1" + 1 + 1); // 111
+console.log(2+ 2 + "1");  // 41
+
+
+
+console.log(true);  // true
+console.log(+true); // 1
+console.log(+"")    // 0   because first "" gives false and than + convert to number as 0
+
+
+
+
+
+
+
+
+
+
+
